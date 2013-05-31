ca0e76657a6a155621adcd1f3f7c6200a0da6154
Patsy Price <patsy.m.price@gmail.com>
2013-05-31 09:47:13 -0700

    update main.rb

--- main.rb
+++ main.rb
@@ -33,10 +33,22 @@ end
 spca = Shelter.new("SPCA", "San Francisco")
 
 # spca pets
-spca.pets["bogey"] = Pet.new("dog","husky","Bogey","male","8",["rabbit","sled"])
-spca.pets["snoopy"] = Pet.new("dog","beagle","Snoopy","male","3",["typewriter"])
-spca.pets["garfield"] = Pet.new("cat","tabby","Garfield","male","5",["lasagne","bed"])
+# attr_accessor :type, :breed, :name, :gender, :age, :toys, :owner
+# spca.pets["bogey"] = Pet.new("dog","husky","Bogey","male","8",["rabbit","sled"])
+# spca.pets["snoopy"] = Pet.new("dog","beagle","Snoopy","male","3",["typewriter"])
+# spca.pets["garfield"] = Pet.new("cat","tabby","Garfield","male","5",["lasagne","bed"])
 
+bogey = Pet.new("dog","husky","Bogey","male","8",["rabbit","sled"])
+snoopy = Pet.new("dog","beagle","Snoopy","male","3",["typewriter"])
+garfield = Pet.new("cat","tabby","Garfield","male","5",["lasagne","bed"])
+grumpy = Pet.new("cat","persian","Grumpy","male","7",["catnip","mice","blogs"])
+spca.pets["bogey"] = bogey
+spca.pets["grumpy"] = grumpy
+spca.pets["garfield"] = garfield
+spca.pets["snoopy"] = snoopy
+
+# hash["key"].attr 
+# obj.hash["key"].attr
 
 # adopted pets
 tigger = Pet.new("cat","tiger","Tigger","female","2",["mouse","scratch post"])
@@ -45,10 +57,11 @@ tigger = Pet.new("cat","tiger","Tigger","female","2",["mouse","scratch post"])
 john = Client.new("John")
 john.pets << tigger
 
+puts "Thank you for visiting the SPCA today. What would you like to do?"
 
-
-print "Choose to (a)dopt a pet  or (r)eturn a pet or (q)uit: "
+print "You can choose to (a)dopt a pet  or (r)eturn a pet or (q)uit: "
 response = gets.chomp.downcase # chomps get rid of enter key 
+puts ""
 puts "Please enter your name: "
 client_name = gets.chomp.downcase
 puts ""
@@ -56,9 +69,15 @@ puts ""
 while response != "q"
 
   if response == "a"
-    # list available pet
-    spca.pets.each {|x, y| puts x}
- 
+    puts "There are #{spca.pets.length.to_s} pets available to adopt."
+    # list available pets
+
+    spca.pets.each {|x, y| puts x }
+
+    #puts spca.pets["bogey"].breed
+    spca.pets["bogey"].breed
+
+    
     # ask which pet do you want to adopt
     puts "Which pet would you like to adopt (select from the list)?"
     pet_name = gets.chomp
@@ -66,19 +85,20 @@ while response != "q"
     # remove this pet from the shelter
     spca.pets.delete(pet_name)
 
-   
+    
 
     # create client
     client_name = Client.new(client_name)
     client_name.pets << pet_name
 
-    puts "Thank you for adopting #{@pet}"
-
-    puts client_name
+    puts "Thank you for adopting a pet from SPCA."
 
   else response == "r"
 
-    # update pet object owner = spca
+    puts "Sorry to hear that you want to return your pet."
+    puts "What is your pet's name?"
+    pet = gets.chomp
+    # update spca to include pet
     # update client.pets array to remove item
     # array.delete(value)
 
